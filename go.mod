02280b4fa526117fe2a6a55cbfbac574cf329785
Asani Hasan <82603969+asanihasan@users.noreply.github.com>
2025-01-10 11:08:00 +0700

    add config

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/asanihasan/sanDB
 
 go 1.21.3
+
+require gopkg.in/yaml.v2 v2.4.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/asanihasan/sanDB/app"
+)
 
 func main() {
-    fmt.Println("Welcome to sanDB!")
+
+	// Load configuration
+	config, err := app.LoadConfig()
+	if err != nil {
+		log.Fatalf("Error loading configuration: %v", err)
+	}
+
+	// Use the loaded configuration
+	fmt.Printf("Server Port: %d\n", config.Server.Port)
+
+	// Example: Start server using the loaded port
+	fmt.Printf("Starting server on port %d...\n", config.Server.Port)
 }
